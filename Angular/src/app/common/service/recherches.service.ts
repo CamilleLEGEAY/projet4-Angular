7f1ebcf7c1248fdfd021504123628367e4f713b2
Camille <camille.legeay@gmail.com>
2020-07-13 01:52:05 +0200

    recherche multicritère ok

--- Angular/src/app/common/service/recherches.service.ts
+++ Angular/src/app/common/service/recherches.service.ts
@@ -2,17 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ReponseApiEtablissements } from '../data/reponses-api';
-import { Recherche } from '../data/recherche';
 
-const urlEtablissement: string = 'https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/?statut_diffusion=O&etat_administratif=A';
+export const urlEtablissement: string = 'https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/?statut_diffusion=O&etat_administratif=A';
 const urlDepartement: string = 'https://geo.api.gouv.fr/departements/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecherchesService {
-
-  reponseAPIconcat: ReponseApiEtablissements = new ReponseApiEtablissements();
 
   constructor(private http: HttpClient) { }
 
@@ -25,140 +22,17 @@ export class RecherchesService {
     let url = urlDepartement;
     return this.http.get<any>(url);
   }
-  /**
-   * J'essaye de remonter this.reponseAPIconcat dans mon component
-   * @param recherche 
-   */
-  async postRecherche(recherche: Recherche): Promise<ReponseApiEtablissements> {
-    this.reponseAPIconcat = new ReponseApiEtablissements();
-    var url = urlEtablissement + "&per_page=20&page=1";
-    let extentionUrl = this.urlLevelOne(recherche);
-    if (extentionUrl != null) {url = url + extentionUrl;}
 
-    if (recherche.departement != null && recherche.code_postal===undefined ||recherche.code_postal==="") {
-      console.log("recherche.cp est null")
-      if (recherche.departement === "2A" || recherche.departement === "2B") {
-        recherche.departement = "20";
-      }
-      await this.searchWithDepartement(recherche,url).then((reponse)=>{
-        return new Promise (function(){this.reponseAPIconcat});
-
-      });
-    }
-    else {
-      if (recherche.effectifs != null) {
-        await this.searchWithEffectif(recherche.effectifs,url).then((reponse)=>{
-          return new Promise (function(){this.reponseAPIconcat});
-
-        });
-      }
-      else {console.log("Je vais doSearch"); await this.doSearch(url).then((reponse)=>{
-        console.log("fin service : "+JSON.stringify(this.reponseAPIconcat));
-        return new Promise (function(){this.reponseAPIconcat});
-
-      });}
-    }
-    console.log("au cas où : "+JSON.stringify(this.reponseAPIconcat));
-    return new Promise (function(){this.reponseAPIconcat});
+  doSearch(urlFinal: string) : Observable<ReponseApiEtablissements> {
+    console.log(urlFinal);
+    return this.http.get<ReponseApiEtablissements>(urlFinal);
   }
-
-
-
-
-
-
-  private async searchWithDepartement(recherche : Recherche, url: string){
-    let listeCP = this.cpDepartement(recherche.departement);
-    if (recherche.effectifs != null) {
-      for (let cp of listeCP) {
-        let urlLevelTwo = url + "&code_postal=" + cp;
-        console.log("urlLevelTwo : "+urlLevelTwo);
-        this.searchWithEffectif(recherche.effectifs,urlLevelTwo)
-      }
-    }  
-    else{
-      for (let cp of listeCP) {
-        let urlLevelTwo = url + "&code_postal=" + cp;
-        console.log("urlLevelTwo : "+urlLevelTwo);
-        this.doSearch(urlLevelTwo)
-      }
-    }
-  }
-
-  private async searchWithEffectif(effectif : number, url: string) {
-    let listeTranche = this.listeTracheEffectif(effectif);
-    for (let tranche of listeTranche) {
-      let urlLastLevel = url + "&tranche_effectifs=" + tranche;
-      console.log("urlLastLevel : "+urlLastLevel);
-      this.doSearch(urlLastLevel);
-    }
-  }
-
-  private async doSearch(urlFinal: string){
-      let result = this.http.get<ReponseApiEtablissements>(urlFinal);
-      result.subscribe(async (reponse) => {
-        this.reponseAPIconcat.meta.total_results = this.reponseAPIconcat.meta.total_results + reponse.meta.total_results;
-        console.log("nbResultat doSearch: " + this.reponseAPIconcat.meta.total_results);
-        this.reponseAPIconcat.etablissements = reponse.etablissements;
-        console.log("end doSearch : "+JSON.stringify(this.reponseAPIconcat));
-      },
-      (err) => { console.log(err); }
-      )  
-      console.log("Je suis au bout : "+JSON.stringify(this.reponseAPIconcat));
-  }
-
-
-
-
-
-
-
-
-
-
-
-
   
-  private urlLevelOne(recherche: Recherche): string {
-    let extentionURL: string;
 
-    if (recherche.siren != null) {
-      let siren = "&siren=" + recherche.siren;
-      extentionURL = extentionURL + siren;
-    }
-    if (recherche.denomination != null) {
-      let denomination = "&denomnation=" + recherche.denomination;
-      extentionURL = extentionURL + denomination;
-    }
-    if (recherche.date_creation != null) {
-      let date_creation = "&date_creation=" + recherche.date_creation;
-      extentionURL = extentionURL + date_creation;
-    }
-    if (recherche.libelle_commune != null) {
-      let libelle_commune = "&libelle_commune=" + recherche.libelle_commune.toUpperCase();
-      extentionURL = extentionURL + libelle_commune;
-    }
-    if (recherche.code_postal != null) {
-      let code_postal = "&code_postal=" + recherche.code_postal;
-      extentionURL = extentionURL + code_postal;
-    }
-    return extentionURL;
-  }
 
-  private listeTracheEffectif(valueEffectifs: number): any {
-    let listeTranche;
-    if (valueEffectifs == 1) { listeTranche = ["00", "01", "02", "03"] }
-    else {if (valueEffectifs == 11) { listeTranche = [11]; }
-      else { if (valueEffectifs == 12) { listeTranche = [12]; }
-        else { if (valueEffectifs == 21) { listeTranche = [21]; }
-          else { if (valueEffectifs == 22) { listeTranche = [22]; }
-            else { if (valueEffectifs == 5) { listeTranche = [31, 32]; }
-              else { listeTranche = [41, 42, 51, 52, 53];}
-    }}}}}
-    return listeTranche;
-  }
-  private cpDepartement(departement: string): string[] {
-    let listeCP : Array<string> = new Array<string>();
+
+  cpDepartement(departement: string): string[] {
+    let listeCP: Array<string> = new Array<string>();
     listeCP.push("77100");
     listeCP.push("30900");
     return listeCP;
--- Angular/src/app/page-resultat/page-resultat.component.ts
+++ Angular/src/app/page-resultat/page-resultat.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { RecherchesService } from '../common/service/recherches.service';
+import { RecherchesService, urlEtablissement } from '../common/service/recherches.service';
 import { Etablissement } from '../common/data/etablissement';
 import { ExportDataService } from '../common/service/export-data.service';
 import { Builder } from '../common/dao/builder';
 import { Recherche } from '../common/data/recherche';
+import { ReponseApiEtablissements } from '../common/data/reponses-api';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-page-resultat',
@@ -13,63 +15,197 @@ import { Recherche } from '../common/data/recherche';
 export class PageResultatComponent implements OnInit {
 
   builder: Builder = new Builder();
-  allEtablissement:Etablissement[];
-  shownEtablissement:Etablissement[];
-  recherche:Recherche= new Recherche();
-  nbResultat : number;
+  allEtablissement: Etablissement[];
+  shownEtablissement: Etablissement[];
+  recherche: Recherche = new Recherche();
+  nbResultat: number;
   departements: any[];
 
-  constructor(private recherchesService : RecherchesService, private exportData : ExportDataService) {   
-    this.start(); 
+
+
+  constructor(private recherchesService: RecherchesService, private exportData: ExportDataService) {
+    this.start();
     this.initDepartements();
   }
-  
+
   ngOnInit(): void {
   }
 
-  start():void{
+  start(): void {
     this.recherchesService.getPageOne()
-    .subscribe(
-      (reponse)=>{
-        if(this.shownEtablissement === undefined){
-        this.shownEtablissement = this.builder.arrayEtablissementBuilder(reponse.etablissements) ;
-        this.nbResultat = reponse.meta.total_results;
-        console.log("liste par défaut chargé");}
-      },
-      (err)=>{console.log(err);
-      }
-    )
+      .subscribe(
+        (reponse) => {
+          if (this.shownEtablissement === undefined) {
+            this.shownEtablissement = this.builder.arrayEtablissementBuilder(reponse.etablissements);
+            this.nbResultat = reponse.meta.total_results;
+            console.log("liste par défaut chargé");
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
   }
 
-  initDepartements():void{
+  /**
+   * fill the selector HTML
+   */
+  initDepartements(): void {
     this.recherchesService.initDepartements()
-    .subscribe(
-      (reponse)=>{
-        this.departements = reponse;
-        console.log(this.departements);
-      },
-      (err)=>{console.log(err);
-      }
-    )
+      .subscribe(
+        (reponse) => {
+          this.departements = reponse;
+          console.log("departements filled");
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
   }
-/**
- * n'attend pas l'async, la reponseAPIconcat du service
- */
-  async onRecherche(){
-    console.log(this.recherche);
-    this.recherchesService.postRecherche(this.recherche).then((resultat)=>{
-    console.log("Je suis component : "+JSON.stringify(resultat));
-    this.shownEtablissement = this.builder.arrayEtablissementBuilder(resultat.etablissements) ;
-    this.nbResultat = resultat.meta.total_results;
-  });
+
+  createExcel() {
+    //this.recherchesService.getExtraction(this.recherche)
+    if (this.allEtablissement[1000000] === null) {
+      this.exportData.exportAsExcelFile(this.allEtablissement, "RLEF");
+    }
   }
   
+  /**
+   * to know how many responses for a research and get first page
+   */
+  onRecherche() {
+    console.log(this.recherche);
+    let url = urlEtablissement + "&per_page=20&page=1";
+    url = this.urlLevelOne(this.recherche, url);
 
-  createExcel(){
-    //this.recherchesService.getExtraction(this.recherche)
-    if (this.allEtablissement[1000000]=== null){
-    this.exportData.exportAsExcelFile(this.allEtablissement,"RLEF");
+    if (this.recherche.departement != null && this.recherche.code_postal === undefined || this.recherche.code_postal === "") {
+      if (this.recherche.departement === "2A" || this.recherche.departement === "2B") {
+        this.recherche.departement = "20";
+      }
+      console.log("I'm doing searchWithDepartement");
+      this.searchWithDepartement(this.recherche, url);
+    }
+    else {
+      if (this.recherche.effectifs != null) {
+        console.log("I'm doing searchWithEffectif");
+        this.searchWithEffectif(this.recherche.effectifs, url);
+      }
+      else {
+        console.log("I'm doing doSearch");
+        this.recherchesService.doSearch(url).subscribe((reponse) => {
+          this.shownEtablissement = this.builder.arrayEtablissementBuilder(reponse.etablissements);
+          this.nbResultat = reponse.meta.total_results;
+        },
+          (err) => { console.log(err); }
+        )
+      }
     }
   }
 
+  /**
+   * fill the url with departement and may be effectif criteria and lauch doSearch
+   * @param effectif 
+   * @param url 
+   */
+  private async searchWithDepartement(recherche: Recherche, url: string) {
+    let observableBatch = [];
+    let reponseAPIconcat: ReponseApiEtablissements = new ReponseApiEtablissements();
+    let listeCP = this.recherchesService.cpDepartement(recherche.departement);
+    if (recherche.effectifs != null) {
+      for (let cp of listeCP) {
+        let urlLevelTwo = url + "&code_postal=" + cp;
+        this.searchWithEffectif(recherche.effectifs, urlLevelTwo);
+      }
+    }
+    else {
+      for (let cp of listeCP) {
+        let urlLevelTwo = url + "&code_postal=" + cp;
+        observableBatch.push(this.recherchesService.doSearch(urlLevelTwo));
+      }
+      forkJoin<ReponseApiEtablissements>(observableBatch).subscribe((reponse) => {
+        for (let rep of reponse) {
+          reponseAPIconcat.meta.total_results = reponseAPIconcat.meta.total_results + rep.meta.total_results;
+          reponseAPIconcat.etablissements = rep.etablissements;
+        }
+        this.shownEtablissement = this.builder.arrayEtablissementBuilder(reponseAPIconcat.etablissements);
+        this.nbResultat = reponseAPIconcat.meta.total_results;
+      });
+    }
+  }
+  /**
+   * fill the url with effectif criteria and lauch doSearch
+   * @param effectif 
+   * @param url 
+   */
+  private async searchWithEffectif(effectif: number, url: string) {
+    let observableBatch = [];
+    let reponseAPIconcat: ReponseApiEtablissements = new ReponseApiEtablissements();
+    let listeTranche = this.listeTrancheEffectif(effectif);
+    for (let tranche of listeTranche) {
+      let urlLastLevel = url + "&tranche_effectifs=" + tranche;
+      observableBatch.push(this.recherchesService.doSearch(urlLastLevel))
+    }
+    forkJoin<ReponseApiEtablissements>(observableBatch).subscribe((reponse) => {
+      for (let rep of reponse) {
+        reponseAPIconcat.meta.total_results = reponseAPIconcat.meta.total_results + rep.meta.total_results;
+        reponseAPIconcat.etablissements = rep.etablissements;
+      }
+      this.shownEtablissement = this.builder.arrayEtablissementBuilder(reponseAPIconcat.etablissements);
+      this.nbResultat = reponseAPIconcat.meta.total_results;
+    });
+  }
+  /**
+   * fill url for research with basic criteria
+   * @param recherche 
+   */
+  private urlLevelOne(recherche: Recherche, url: string): string {
+    let extentionURL = url;
+
+    if (recherche.siren != null) {
+      let siren = "&siren=" + recherche.siren;
+      extentionURL = extentionURL + siren;
+    }
+    if (recherche.denomination != null) {
+      let denomination = "&denomnation=" + recherche.denomination;
+      extentionURL = extentionURL + denomination;
+    }
+    if (recherche.date_creation != null) {
+      let date_creation = "&date_creation=" + recherche.date_creation;
+      extentionURL = extentionURL + date_creation;
+    }
+    if (recherche.libelle_commune != null) {
+      let libelle_commune = "&libelle_commune=" + recherche.libelle_commune.toUpperCase();
+      extentionURL = extentionURL + libelle_commune;
+    }
+    if (recherche.code_postal != null) {
+      let code_postal = "&code_postal=" + recherche.code_postal;
+      extentionURL = extentionURL + code_postal;
+    }
+    return extentionURL;
+  }
+  /**
+   * prepare the the research with the effectif criteria
+   * @param valueEffectifs 
+   */
+  private listeTrancheEffectif(valueEffectifs: number): any {
+    let listeTranche;
+    if (valueEffectifs == 1) { listeTranche = ["00", "01", "02", "03"] }
+    else {
+      if (valueEffectifs == 11) { listeTranche = [11]; }
+      else {
+        if (valueEffectifs == 12) { listeTranche = [12]; }
+        else {
+          if (valueEffectifs == 21) { listeTranche = [21]; }
+          else {
+            if (valueEffectifs == 22) { listeTranche = [22]; }
+            else {
+              if (valueEffectifs == 5) { listeTranche = [31, 32]; }
+              else { listeTranche = [41, 42, 51, 52, 53]; }
+            }
+          }
+        }
+      }
+    }
+    return listeTranche;
+  }
 }
