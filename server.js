5d49b84d1e3d5cbda6e27bd5833c165f6a261a6e
Camille <camille.legeay@gmail.com>
2020-07-18 17:34:49 +0200

    tuto server + dependencies

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors'); //<-- required installing 'cors' (npm i cors --save)
-app.use(cors());
-
-app.use(express.static('./dist/projet4-angular'));
+app.use(express.static(__dirname+'./dist/projet4-angular'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/projet4-angular/'});
+    res.sendFile(path.join(__dirname+'/dist/projet4-angular/'));
   });
 
 app.listen(process.env.PORT || 8080);
