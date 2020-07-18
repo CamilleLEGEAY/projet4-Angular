4eeca5a83a89feaba39b6e6384960bba36585ac7
Camille <camille.legeay@gmail.com>
2020-07-18 14:28:32 +0200

    installation cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express();
+
+const cors = require('cors'); //<-- required installing 'cors' (npm i cors --save)
+app.use(cors());
 
 app.use(express.static('./dist/projet4-angular'));
 app.get('/*', function(req, res) {
