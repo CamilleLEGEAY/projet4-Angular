fc38fae4706522cf64df2531ff59aca664b61b04
Camille <camille.legeay@gmail.com>
2020-07-21 13:24:35 +0200

    environnement et favicon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  msLogin: 'http://localhost:9999', 
+  msStats: 'http://localhost:9996',
+  msSauvegarde: 'http://localhost:9995',
+  urlEtablissement: 'https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/?statut_diffusion=O&etat_administratif=A',
+  urlDepartement: 'https://geo.api.gouv.fr/departements/'
 };
 
 /*
