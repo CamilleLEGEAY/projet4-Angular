621965e7bbc4ff78233a303a0cb910d0bc816ada
Camille <camille.legeay@gmail.com>
2020-07-21 16:36:34 +0200

    yml

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('./dist/rlef'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/rlef/'});
-  });
-
-app.listen(8080);
